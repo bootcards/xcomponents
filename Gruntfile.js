f73241b0c88121f0eedbf5563d1038d2e74c5d47
Mark Leusink <m.leusink@gmail.com>
2015-03-05 11:12:34 +0100

    FastClick dependency replaced by ng-touch

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,6 +39,7 @@ module.exports = function(grunt) {
           'bower_components/angular-resource/angular-resource.min.js',
           'bower_components/angular-animate/angular-animate.min.js',
           'bower_components/angular-sanitize/angular-sanitize.min.js',
+          'bower_components/angular-touch/angular-touch.min.js',
           'bower_components/angular-bootstrap/ui-bootstrap.min.js',
           'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js'
 
