49b8711348e3510c3c43bb069fd85466e504eb82
Mark Leusink <m.leusink@gmail.com>
2015-03-06 11:10:00 +0100

    fix for bootcards/xcomponents-demoapp#17 and bootcards/xcomponents-demoapp#23

--- src/main.js
+++ src/main.js
@@ -135,3 +135,12 @@ app.run( function() {
 	
 });
 
+app.directive('disableNgAnimate', ['$animate', function($animate) {
+  return {
+    restrict: 'A',
+    link: function(scope, element) {
+      $animate.enabled(false, element);
+    }
+  };
+}]);
+
